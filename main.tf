29b240957800e4a6f3d9ea7240958c08f167f001
onokatio <onokatio@gmail.com>
2019-12-28 18:16:43 +0900

    finish

--- main.tf
+++ main.tf
@@ -11,14 +11,44 @@ resource "n0stack_network" "f35" {
 }
 
 variable "machines" {
-	default = {
-	}
+	type = list(object({
+		vm_name = string
+		address = string
+	}))
+	default = [
+		{
+			vm_name = "f35-manager-1"
+			address = "192.168.7.1"
+		},
+		{
+			vm_name = "f35-manager-2"
+			address = "192.168.7.2"
+		},
+		{
+			vm_name = "f35-manager-3"
+			address = "192.168.7.3"
+		},
+		{
+			vm_name = "f35-worker-1"
+			address = "192.168.7.4"
+		},
+		{
+			vm_name = "f35-worker-2"
+			address = "192.168.7.5"
+		},
+		{
+			vm_name = "f35-worker-3"
+			address = "192.168.7.6"
+		},
+	]
 }
 
-resource "n0stack_blockstorage" "manager-1" {
+resource "n0stack_blockstorage" "blockstorage" {
+	count = length(var.machines)
+
 	image_name = "baseimage-ubuntu"
 	tag = "latest"
-	blockstorage_name = "f35-manager-1"
+	blockstorage_name = var.machines[count.index].vm_name
 	annotations = {
 		"n0core/provisioning/block_storage/request_node_name" = "n0node00"
 	}
@@ -26,19 +56,20 @@ resource "n0stack_blockstorage" "manager-1" {
 	limit_bytes = 10737418240
 }
 
-resource "n0stack_virtualmachine" "manager-1" {
-	name = "f35-manager-1"
-	annotations = {
-		"n0core/provisioning/virtual_machine/request_node_name" = "n0node00"
-	}
-	request_cpu_milli_core = 10
-	limit_cpu_milli_core = 1000
-	request_memory_bytes = 536870912
-	limit_memory_bytes = 536870912
-	block_storage_names = [n0stack_blockstorage.manager-1.blockstorage_name]
+resource "n0stack_virtualmachine" "vitualmachine" {
+	count = length(var.machines)
+	name = var.machines[count.index].vm_name
+        annotations = {
+                "n0core/provisioning/virtual_machine/request_node_name" = "n0node00"
+        }
+        request_cpu_milli_core = 10
+        limit_cpu_milli_core = 1000
+        request_memory_bytes = 536870912
+        limit_memory_bytes = 536870912
+        block_storage_names = [var.machines[count.index].vm_name]
 
-	nics {
-		network_name = n0stack_network.f35.name
-		ipv4_address = "192.168.7.1"
-	}
+        nics {
+                network_name = n0stack_network.f35.name
+                ipv4_address = var.machines[count.index].address
+        }
 }
